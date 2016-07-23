72ce8eb977343dcf51149d68d794e4d297c1ad24
rushibadami1992 <rushibadami1992@gmail.com>
2016-07-23 16:45:09 -0400

    Structure Queue

---
+++ Queues/LinkedQueue.java
@@ -0,0 +1,52 @@
+
+public class LinkedQueue{
+private int length;
+private ListNode front,rear;
+
+public LinkedQueue(){
+length=0;
+front=rear=null;
+}
+
+public void enqueue(int data)
+{
+ListNode node=new ListNode(data);
+if(isEmpty())
+	{
+		front =node;
+	}
+else
+	{
+	rear.setNext(node);
+	rear=node;
+	rear=node;
+	length++;	
+	}
+}
+
+public int dequeue() throws Exception
+{
+	if(isEmpty())
+	{
+	throw new Exception();		
+	}
+		int result=front.getData();
+		front=front.getNext();
+		length--;
+	if(isEmpty())
+	{
+		rear=null;
+	}
+	return result;
+}
+public boolean isEmpty()
+{
+ 	return (length==0);
+}	
+ public int size()
+{
+return length;
+}
+}
+
+
