113b30a1a707452219a5ed35e75c0ddaae116263
rushibadami1992 <rushibadami1992@gmail.com>
2016-09-24 23:49:58 -0400

    Merge Linked List

--- Trees/MergeLinkedList.java
+++ Trees/MergeLinkedList.java
@@ -10,5 +10,38 @@ public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 			return l2;
 		}
 	}
+
+ public ListNode swapPairs(ListNode node) {
+         if (node == null || node.next == null) {
+            return node;
+        }
+ 
+        // Initialize previous and current pointers
+        ListNode prev = node;
+        ListNode curr = node.next;
+ 
+        node = curr;  // Change head before proceeeding
+ 
+        // Traverse the list
+        while (true) {
+            ListNode next = curr.next;
+            curr.next = prev; // Change next of current as previous node
+ 
+            // If next NULL or next is the last node
+            if (next == null || next.next == null) {
+                prev.next = next;
+                break;
+            }
+ 
+            // Change next of previous to next next
+            prev.next = next.next;
+ 
+            // Update previous and curr
+            prev = next;
+            curr = prev.next;
+}
+
+return node;
+}
 }
 
